c58cad8fec89b2e908ec44d5a766561935d9bc93
WahabBello <abdouwahab123@gmail.com>
2025-10-03 01:38:15 +0200

    configure Vite with Vuetify plugin and TypeScript

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,7 +1,8 @@
+// @ts-ignore
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
 export default createVuetify({
